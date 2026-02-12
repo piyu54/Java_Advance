159b0685b0d3880d09e414ab6355bd60eccb294d
unknown <priyakushwaha1407@gmail.com>
2026-02-12 19:11:21 +0530

    commit

--- src/com/jdbc/first/DemoFirst.java
+++ src/com/jdbc/first/DemoFirst.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DemoFirst {
-
+//starting
 	public static void main(String[] args) throws  Exception {
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
