7990a9292c7bd2673ff882dcc88b49e7e6112eb5
unknown <priyakushwaha1407@gmail.com>
2026-02-13 22:57:38 +0530

    commit

--- src/com/jdbc/first/Fetch.java
+++ src/com/jdbc/first/Fetch.java
@@ -7,15 +7,15 @@ import java.sql.Statement;
 public class Fetch {
 public static void main(String[] args) throws Exception {
 		
-//		Class.forName("com.mysql.cj.jdbc.Driver");
-//		
-//		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBC", "root", "root");
-//		
-//		Statement s=c.createStatement();
-//		
-//		s.executeQuery("select*from student");
-//		
-//		System.out.println("not succeess");
-//		c.close();
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBC", "root", "root");
+		
+		Statement s=c.createStatement();
+		
+		s.executeQuery("select*from student");
+		
+		System.out.println(" succeess");
+		c.close();
 }
 }
