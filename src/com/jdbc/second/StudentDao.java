0919fe1eac1752a3233540616b603c5ea9af82c9
unknown <priyakushwaha1407@gmail.com>
2026-02-13 23:13:09 +0530

    commit

---
+++ src/com/jdbc/second/StudentDao.java
@@ -0,0 +1,9 @@
+package com.jdbc.second;
+
+public class StudentDao {
+
+	public void fetchAllData() throws Exception
+	{
+		Class.forName("com.sql.cj.jdbc.Driver");
+	}
+}
---
+++ src/com/jdbc/second/StudentService.java
@@ -0,0 +1,5 @@
+package com.jdbc.second;
+
+public class StudentService {
+
+}
