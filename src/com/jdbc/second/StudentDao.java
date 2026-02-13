168c8df890ce6ce4f335d210559a86153ede52d6
unknown <priyakushwaha1407@gmail.com>
2026-02-13 23:24:58 +0530

    commit

--- src/com/jdbc/second/StudentDao.java
+++ src/com/jdbc/second/StudentDao.java
@@ -1,9 +1,26 @@
 package com.jdbc.second;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class StudentDao {
 
 	public void fetchAllData() throws Exception
 	{
 		Class.forName("com.sql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBC", "root", "root");
+		
+		Statement s=con.createStatement();
+		
+		ResultSet c=s.executeQuery("select * from student");
+		
+		while(c.next())
+		{
+			System.out.println();
+		}
+		
+		con.close();
 	}
 }
